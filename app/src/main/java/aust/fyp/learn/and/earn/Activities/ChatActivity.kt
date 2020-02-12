1eff5f43e6a0835f00add79a8523670d69266d2d
Abdullah Zeb <abdullahzeb1999@gmail.com>
2020-02-12 15:34:37 +0500

    code by abdullah

--- app/src/main/java/aust/fyp/learn/and/earn/Activities/ChatActivity.kt
+++ app/src/main/java/aust/fyp/learn/and/earn/Activities/ChatActivity.kt
@@ -2,13 +2,17 @@ package aust.fyp.learn.and.earn.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import aust.fyp.learn.and.earn.R
+import aust.fyp.learn.and.earn.StoreRoom.PreferenceManager
 import aust.fyp.learn.and.earn.StoreRoom.SocketConnectionHandler
+import com.github.nkzawa.socketio.client.Ack
 import com.github.nkzawa.socketio.client.Socket
+import org.json.JSONObject
 
 class ChatActivity : AppCompatActivity() {
 
@@ -17,6 +21,7 @@ class ChatActivity : AppCompatActivity() {
     lateinit var receiver_name: TextView
     var receiver_id: Int = 0
     var socket: Socket? = null
+    var TAG = "ChatActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +39,44 @@ class ChatActivity : AppCompatActivity() {
         receiver_id = intent!!.getIntExtra("ID", 0)
     }
 
-    fun send_message(view: View) {
+    fun sendMessage(view: View) {
+        var message_str = message.text.toString().trim()
+        if (!message_str.isEmpty()) {
+            try {
 
+                var json = JSONObject()
+                json.put(
+                    "sender_id",
+                    PreferenceManager.getInstance(applicationContext!!)!!.getUserId()
+                )
+                json.put(
+                    "sender_name",
+                    PreferenceManager.getInstance(applicationContext!!)!!.getUserName()
+                )
+                json.put(
+                    "receiver_id",
+                    receiver_id
+                )
+                json.put(
+                    "receiver_name",
+                    receiver_name.text.toString().trim()
+                )
+                json.put(
+                    "message",
+                    message_str
+                )
+
+                socket!!.emit("send-message", json, Ack { args ->
+                    runOnUiThread {
+                        var data = args[0] as JSONObject
+                        Log.i(TAG, "{$data}")
+                        message.setText("")
+                    }
+                })
+
+            } catch (e: Exception) {
+                Log.i(TAG, "exception: $e")
+            }
+        }
     }
 }
